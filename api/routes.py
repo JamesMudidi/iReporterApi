f1cfb49196272def16d5dfb80924fe2c05245b5e
James Mudidi <mudidi.jimmy@gmail.com>
2018-11-27 16:53:50 +0300

    Updated test_routes.py

--- api/routes.py
+++ api/routes.py
@@ -43,7 +43,7 @@ def login():
 
     if not error:
         if username != None:
-            if password['password'] == password and user['username'] == username:
+            if user['password'] == password and user['username'] == username:
                 token = create_access_token(username)
                 return jsonify ({
                     'access_token': token,
