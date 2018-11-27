6f39fef9f0fa46a34374ad732a10dae48fcbafc5
James Mudidi <mudidi.jimmy@gmail.com>
2018-11-27 22:41:53 +0300

    Updated test_routes.py

--- api/models.py
+++ api/models.py
@@ -12,8 +12,6 @@ class Users:
     def check_user_exist(self):
         if username != None:
             return 'Username is taken.'
-        if email != None:
-            return 'Email already has an account.'
 
     def validate_input(self):			
         if not username or username.isspace():
@@ -23,5 +21,3 @@ class Users:
     def login_validate(username, password):
         if not username or username.isspace():
             return 'Username field can not be left empty.'
-        elif not password or password.isspace():
-            return 'Password field can not be left empty.'
