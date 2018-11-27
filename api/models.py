450316c42753c2bf2116e5fba5bc7aaf23307aee
James Mudidi <mudidi.jimmy@gmail.com>
2018-11-27 19:03:23 +0300

    Updated test_routes.py

--- api/models.py
+++ api/models.py
@@ -16,9 +16,9 @@ class Users:
             return 'Email already has an account.'
 
     def validate_input(self):			
-        if len(self.password) < 4:
-            return 'Password has to be longer than 4 characters.'
-    
+        if not self or self.isspace():
+            return 'Fields can not be left empty.'
+			    
     @staticmethod
     def login_validate(username, password):
         if not username or username.isspace():
