310a5fffc62efa495eaf8adfc596b3ad2f5877cc
James Mudidi <mudidi.jimmy@gmail.com>
2018-11-27 21:46:38 +0300

    Updated test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -25,7 +25,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['James successfully registered.'])
+        self.assertIn(message['James successfully registered.'])
     
     def test_register_username_twice(self):
         user1 = {
@@ -60,7 +60,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response2.data.decode())
 
-        self.assertEqual(message['Username is taken.'])
+        self.assertIn(message['Username is taken.'])
 
     def test_register_email_twice(self):
         user1 = {
@@ -93,7 +93,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response2.data.decode())
 
-        self.assertEqual(message['Email already has an account.'])
+        self.assertIn(message['Email already has an account.'])
 
     def test_register_empty_username(self):
         user = {
@@ -112,7 +112,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['Username field can not be left empty.'])
+        self.assertIn(message['Username field can not be left empty.'])
 
     def test_register_empty_email(self):
         user = {
@@ -132,7 +132,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['Email field can not be left empty.'])
+        self.assertIn(message['Email field can not be left empty.'])
 
     def test_register_empty_password(self):
         user = {
@@ -152,7 +152,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['Password field can not be left empty.'])
+        self.assertIn(message['Password field can not be left empty.'])
 
     def test_register_invalid_email(self):
         user = {
@@ -172,7 +172,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['Enter a valid email address.'])
+        self.assertIn(message['Enter a valid email address.'])
 
     def test_register_password_length(self):
         user = {
@@ -192,7 +192,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['Password has to be longer than 4 characters.'])
+        self.assertIn(message['Password has to be longer than 4 characters.'])
 
     def test_user_login(self):
         user1 = {
@@ -223,7 +223,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['james successfully logged in.'])
+        self.assertIn(message['james successfully logged in.'])
 
     def test_user_login_empty_username(self):
         user1 = {
@@ -254,7 +254,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['Username field can not be left empty.'])
+        self.assertIn(message['Username field can not be left empty.'])
 
     def test_user_login_empty_password(self):
         user1 = {
@@ -285,7 +285,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['Password field can not be left empty.'])
+        self.assertIn(message['Password field can not be left empty.'])
     
     def test_login_wrong_username(self):
         user1 = {
@@ -316,7 +316,7 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['Wrong login credentials.'])
+        self.assertIn(message['Wrong login credentials.'])
 
     def test_login_wrong_password(self):
         user1 = {
@@ -347,5 +347,5 @@ class TestUsers(unittest.TestCase):
 
         message = json.loads(response.data.decode())
 
-        self.assertEqual(message['Wrong login credentials.'])
+        self.assertIn(message['Wrong login credentials.'])
     
