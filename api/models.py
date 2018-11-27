eee6d07d8e19146fc98bcffd1509af3bded04cf1
James Mudidi <mudidi.jimmy@gmail.com>
2018-11-27 19:08:50 +0300

    Updated test_routes.py

--- api/models.py
+++ api/models.py
@@ -16,7 +16,7 @@ class Users:
             return 'Email already has an account.'
 
     def validate_input(self):			
-        if not self():
+        if self.isspace():
             return 'Fields can not be left empty.'
 			    
     @staticmethod
