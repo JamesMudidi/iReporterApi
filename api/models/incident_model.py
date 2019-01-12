39ce4620d5bfe50e22c5722e16994b0eb2982960
James Mudidi <mudidi.jimmy@gmail.com>
2019-01-12 20:41:00 +0300

    working on incident and user models to refactor code and use best practices

--- api/models/incident_model.py
+++ api/models/incident_model.py
@@ -1,15 +1,25 @@
+'''
+Import relevant dependencies
+datetime for the date and time
+'''
+from flask import jsonify
+import datetime
+
+# Create a list to save the created incidents
 incidents=[]
+
+# Defining model class for incidents
 class Incident:
     def __init__(self):
         self.incidents=incidents
 
     def create_incident(self,args):
         incident=dict(
-            _id= len(self.incidents)+1,
-            createdOn=args['createdOn'],
+            incidentId=len(incidents)+1,
+            createdOn=datetime.datetime.now(),
             createdBy=args['createdBy'],
-            type=args['type'],
-            location=args['location'],
+            incidentType=args['incidentType'],
+            location=geocoder.ip('me'),
             status=args['status'],
             image=args['image'],
             video=args['video'],
@@ -21,7 +31,29 @@ class Incident:
     def get_incidents(self):
         return self.incidents
 
-    def get_an_incident(self,_id):
+    def get_an_incident(self,incidentId):
         for incident in self.incidents:
-            if incident['_id']==_id:
+            if incident['incidentId']==incidentId:
                 return incident
+
+    def get_redflags(self,incidentType):
+        for incident in self.incidents:
+            if incident['incidentType']==Redflag:
+                return incident
+
+    def get_interventions(self,incidentType):
+        for incident in self.incidents:
+            if incident['incidentType']==Intervention:
+                return incident
+
+# Defining model class for the Redflag and inheriting the incident class
+class Redflag(Incident):
+    def __init__(self):
+        super().__init__(args)
+        self.incidentType=Redflag
+
+# Defining model class for the Redflag and inheriting the incident class
+class Interventon(Incident):
+    def __init__(self):
+        super().__init__(args)
+        self.incidentType=Interventon
--- api/models/user_model.py
+++ api/models/user_model.py
@@ -1,23 +1,37 @@
+'''
+Import relevant dependencies
+datetime for the date and time
+'''
+import datetime
+from flask import jsonify
+
+# Create a list to save the created users
 users=[]
 
+# Defining model class for users
 class User:
     def __init__(self):
         self.users=users
 
     def create_user(self, args):
         user=dict(
-            user_id=len(self.users)+1,
-            firstname=args['firstname'],
-            lastname=args['lastname'],
-            othernames=args['othernames'],
+            userId=len(self.users)+1,
+            firstName=args['firstName'],
+            lastName=args['lastName'],
+            otherNames=args['otherNames'],
             email=args['email'],
-            phone_number=args['phone_number'],
+            phoneNumber=args['phoneNumber'],
             username=args['username'],
-            registered=args['registered'],
-            is_admin=args['is_admin']
+            registered=datetime.datetime.now(),
+            isAdmin=False
         )
         self.users.append(user)
         return user
 
-    # def get_user(self):
-    #     return self.users
+    def check_user_exists(userId, firstName, lastName, email):
+        #check if a user is already in the system
+        for user in user_list:
+            return user['userId'] == userId
+            return user['firstName'] == firstName
+            return user['lastName'] == lastName
+            return user['email'] == email
