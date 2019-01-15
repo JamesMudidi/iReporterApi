ea4cb3fff2ee5cd7aa8eb48dea856e359b77696f
James Mudidi <mudidi.jimmy@gmail.com>
2019-01-15 18:39:42 +0300

    working on LFA feed back

--- tests/test_incident.py
+++ tests/test_incident.py
@@ -5,13 +5,7 @@ from api import app
 class test_incident(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
-
-    def test_create_incident(self):
-        incident=[]
-        response=self.client.post(
-        '/api/v1/incident',
-        content_type='application/json')
-        data={
+        self.data={
             "id" : 1,
             "createdOn" : "Date",  
             "createdBy" : "James", 
@@ -22,61 +16,46 @@ class test_incident(unittest.TestCase):
             "Images" : "images",
             "Videos" : "videos"
         }
-        incident.append(data)
+
+    def test_create_incident(self):
+        incident=[]
+        response=self.client.post(
+        '/api/v1/incident',
+        content_type='application/json')
+        
+        incident.append(self.data)
         response=self.client.get('/api/v1/incident',
         content_type='application/json')
         msg = json.loads(response.data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
     def test_get_incidents(self):
         # Tests that the end point fetches all records
         response=self.client.get('/api/v1/incident',
         content_type='application/json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
     def test_get_single_incident(self):
         # Tests that the end point returns a single record
-        incident_details = {
-            "id" : 1,
-            "createdOn" : "Date",  
-            "createdBy" : "James", 
-            "type" : "redflag",       
-            "location" : "Kampala",   
-            "status" : "draft",     
-            "comment" : "it starts here",
-            "Images" : "images",
-            "Videos" : "videos"
-            }
         self.client.post('/api/v1/incident',
-        json=incident_details)
+        json=self.data)
         response = self.client.get('/api/v1/incident',
         content_type='application/json')
         msg = json.loads(response.data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
     def test_delete_incident(self):
         # Tests that the end point enables user edit an incident
         # record when rejected by admin
-        incident_details = {
-            "id" : 1,
-            "createdOn" : "Date",  
-            "createdBy" : "James", 
-            "type" : "redflag",       
-            "location" : "Kampala",   
-            "status" : "draft",     
-            "comment" : "it starts here",
-            "Images" : "images",
-            "Videos" : "videos"
-            }
         response = self.client.post('/api/v1/incident',
         content_type='application/json',
-        json=incident_details)
+        json=self.data)
         new_details = {}
         response = self.client.delete('/api/v1/incident/1',
         json=new_details)
         msg = json.loads(response.data)
         self.assertIn("incident successfully deleted", msg['message'])
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
 
     def test_create_redflag(self):
@@ -84,69 +63,39 @@ class test_incident(unittest.TestCase):
         response=self.client.post(
         '/api/v1/redflag',
         content_type='application/json')
-        data={
-            "id" : 1,
-            "createdOn" : "Date",  
-            "createdBy" : "James", 
-            "type" : "red-flags",       
-            "location" : "Kampala",   
-            "status" : "draft",     
-            "comment" : "it starts here",
-            "Images" : "images",
-            "Videos" : "videos"
-        }
-        incident.append(data)
+
+        incident.append(self.data)
         response=self.client.get('/api/v1/incident',
         content_type='application/json')
         msg = json.loads(response.data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
     def test_get_redflags(self):
         # Tests that the end point fetches all records
         response=self.client.get('/api/v1/incident',
         content_type='application/json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
     def test_get_single_redflag(self):
         # Tests that the end point returns a single record
-        redflag = {
-            "id" : 1,
-            "createdOn" : "Date",  
-            "createdBy" : "James", 
-            "type" : "redflag",       
-            "location" : "Kampala",   
-            "status" : "draft",     
-            "comment" : "it starts here",
-            "Images" : "images",
-            "Videos" : "videos"
-            }
+
         self.client.post('/api/v1/redflag',
-        json=redflag)
+        json=self.data)
         response = self.client.get('/api/v1/redflag',
         content_type='application/json')
         msg = json.loads(response.data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
     def test_delete_redflag(self):
         # Tests that the end point enables user edit an incident
         # record when rejected by admin
-        redflag = {
-            "id" : 1,
-            "createdOn" : "Date",  
-            "createdBy" : "James", 
-            "type" : "redflag",       
-            "location" : "Kampala",   
-            "status" : "draft",     
-            "comment" : "it starts here",
-            "Images" : "images",
-            "Videos" : "videos"
-            }
+
         response = self.client.post('/api/v1/redflag',
         content_type='application/json',
-        json=redflag)
+        json=self.data)
         redflag = {}
         response = self.client.delete('/api/v1/redflag/1',
         json=redflag)
         msg = json.loads(response.data)
         self.assertIn("redflag successfully deleted", msg['message'])
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
--- tests/test_user.py
+++ tests/test_user.py
@@ -5,68 +5,51 @@ from flask import Flask
 
 class Test_record_views(unittest.TestCase):
     def setUp(self):
-        self.client = app.test_client()
+        self.client=app.test_client()
+        self.user={
+            "firstname": "test_firstname",
+            "lastname": "test_lastname",
+            "othernames": "test_othernames",
+            "email": "test_email@email.com",
+            "phoneNumber": "test_phonNumber",
+            "username": "test_username",
+            "registered": "registered",
+            "password": "1234",
+        }
 
     def test_register_user(self):
         # Tests that the end point enables a new user create an account
-        user_details = {
-                        "firstname": "test_firstname",
-                        "lastname": "test_lastname",
-                        "othernames": "test_othernames",
-                        "email": "test_email@email.com",
-                        "phoneNumber": "test_phonNumber",
-                        "username": "test_username",
-                        "registered": "registered",
-                        "password": "1234",
-                        }
+        
         response = self.client.post('api/v1/users',
-        json=user_details)
+        json=self.user)
         msg = json.loads(response.data)
-        self.assertEqual(response.status_code, 422)
+        self.assertEqual(422, response.status_code)
 
     def test_fetch_all_users(self):
         # Tests that the end point fetches all users
         response = self.client.get('/api/v1/users',
                                    content_type='application/json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
     def test_fetch_single_user_details(self):
         # Tests that the end point returns a single user's details
-        user_details = {
-                        "firstname": "test_firstname",
-                        "lastname": "test_lastname",
-                        "othernames": "test_othernames",
-                        "email": "test_email@email.com",
-                        "phoneNumber": "test_phonNumber",
-                        "username": "test_username",
-                        "registered": "registered",
-                        "password": "1234",
-                            }
+
         self.client.post('api/v1/users',
-        json=user_details)
+        json=self.user)
         response=self.client.get('/api/v1/users/',
         content_type='application/json')
         msg = json.loads(response.data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
     def test_delete_user_details(self):
         # Tests that the end point enables user delete account
-        user_details = {
-                        "firstname": "test_firstname",
-                        "lastname": "test_lastname",
-                        "othernames": "test_othernames",
-                        "email": "test_email@email.com",
-                        "phoneNumber": "test_phonNumber",
-                        "username": "test_username",
-                        "registered": "registered",
-                        "password": "1234",
-            }
+
         response = self.client.post('api/v1/users',
         content_type='application/json',
-        json=user_details)
+        json=self.user)
         new_details = {}
         response = self.client.delete('api/v1/users/1',
         json=new_details)
         msg = json.loads(response.data)
         self.assertIn("Index out of range", msg['message'])
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(400, response.status_code)
