c5efcad859fa1a0aaa488d57d34455e44ccb3bfb
James Mudidi <mudidi.jimmy@gmail.com>
2019-01-16 01:55:32 +0300

    modifying code to pep8 standards

--- api/__init__.py
+++ api/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def test():
-	return jsonify(
-        {'Application' : 'iRepoter'},
-        {'1. Greetings': 'Welcome to the iRepoter API'},
-        {'2. Help': 'For further assistance contact James Mudidi on mudidi.jimmy@gmail.com.',
+    return jsonify(
+        {'Application': 'iRepoter'},
+            {'1. Greetings': 'Welcome to the iRepoter API'},
+                {'2. Help': 'For further assistance contact James Mudidi on mudidi.jimmy@gmail.com.',
         })
 
 app.url_map.strict_slashes = False
