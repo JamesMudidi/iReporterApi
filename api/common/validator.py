20cecd173127983ed0a69f3ddbb1a5f76c87b326
James Mudidi <39102955+JamesMudidi@users.noreply.github.com>
2019-01-16 21:57:03 +0300

    update validation file to pep8 standards

--- api/common/validator.py
+++ api/common/validator.py
@@ -1,15 +1,17 @@
 import re
 from marshmallow import ValidationError
 
+
 def required(value):
     """Validate that field under validation does not contain null value."""
-    
+
     if isinstance(value, str):
         if not value.strip(' '):
             raise ValidationError('The parameter cannot be null')
         return value
     elif value:
         return value
+
 
 def email(value):
     """Validate field matches email format."""
