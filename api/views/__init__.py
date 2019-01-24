a5c8e1ea3246b77a57a3297e9bde4134ac792567
James Mudidi <mudidi.jimmy@gmail.com>
2019-01-24 05:27:57 +0300

    adding travis badge to README.md

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_swagger_ui import get_swaggerui_blueprint
 
-swagger_blueprint = get_swaggerui_blueprint(SWAGGER_URL='api/v2/docs', API_URL='https://app.swaggerhub.com/apis-docs/JamesMudidi/iRepoter/2.0'
 
 api = Blueprint('api', __name__)
 app.Blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+swagger_blueprint = get_swaggerui_blueprint(SWAGGER_URL='api/v2/docs', API_URL='https://app.swaggerhub.com/apis-docs/JamesMudidi/iRepoter/2.0'
